23c6fdceb7b88e1098a6107c7f39c3849204ec22
Cedric Steiner <c.steiner@taktwerk.ch>
2023-02-16 01:04:04 +0100

    optimize photobooth, add installer

---
+++ install_gphoto.sh
@@ -0,0 +1,2 @@
+wget https://raw.githubusercontent.com/gonzalo/gphoto2-updater/master/gphoto2-updater.sh && wget https://raw.githubusercontent.com/gonzalo/gphoto2-updater/master/.env && chmod +x gphoto2-updater.sh && sudo ./gphoto2-updater.sh
+sudo chmod +s /usr/local/bin/gphoto2
--- web/index.php
+++ web/index.php
@@ -1,4 +1,9 @@
 <?php
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL & ~E_NOTICE);
+
 require __DIR__ . '/vendor/autoload.php';
 
 use BenMajor\ImageResize\Image;
@@ -74,9 +79,19 @@ if(isset($_GET['flashdown'])) {
     echo $result;
     exit;
 }
+if(isset($_GET['flashup'])) {
+    $result = runcmd('flashup');
+    echo $result;
+    exit;
+}
 function runcmd($cmd, $full = false) {
-    if($cmd == 'flashup') {
-        $cmd = 'ola_streaming_client -d ' . getconfig('flash_brightness') . ',' . getconfig('flash_color_r') . ',' . getconfig('flash_color_g') . ',' . getconfig('flash_color_b') . ' 1';
+    if($cmd == 'flashup_preview' || $cmd == 'flashup') {
+        $brightness = getconfig('flash_brightness');
+        if($cmd == 'flashup_preview' ) {
+            $brightness = getconfig('flash_brightness_preview');
+        } 
+
+        $cmd = 'ola_streaming_client -d ' . $brightness . ',' . getconfig('flash_color_r') . ',' . getconfig('flash_color_g') . ',' . getconfig('flash_color_b') . ' 1';
         $full = true;
     }
     else if ($cmd == 'capture') {
@@ -128,7 +143,7 @@ function getconfig($attribute) {
     return $config[$attribute];
 }
 function getCameraImage() {
-    foreach(glob(getconfig('imagepath') . '/tmp/*.jpg') as $image) {
+    foreach(glob(getconfig('imagepath') . '/tmp/*.[jJ][pP][gG]') as $image) {
         break;
     }
     return $image;
@@ -161,6 +176,10 @@ function getCameraImage() {
             background: black;
             color: white;
             height: 100%;
+        }
+        video {
+        -webkit-transform: scaleX(-1);
+        transform: scaleX(-1);
         }
         #camPreview {
             position: fixed;
@@ -206,21 +225,25 @@ function getCameraImage() {
         <div class="container text-center">
             <h1><?= getconfig('title'); ?></h1>
             <p><?= getconfig('description'); ?></p>
-            <a class="<?= $btnClass ?>" href="?take">Zur Kamera-Vorschau</a>
+            <a class="<?= $btnClass ?>" href="?take"><?= getconfig('text_to_preview') ?></a>
         </div>
     </div>
 <?php elseif(isset($_GET['take'])) : ?>
+    <meta http-equiv="refresh" content="<?= getconfig('refresh_take'); ?>; URL=/">
     <?php
-    $result = runcmd('flashup');
+    $result = runcmd('flashup_preview');
     ?><div style="height:100%" class="d-flex align-items-center">
-        <div id="preview-wait" class="container text-center">Vorschau ladet...</div>
-        <div id="prepare-wait" style="font-size: 200%; display: none;" class="container text-center">Einen Moment...</div>
+        <div id="preview-wait" class="container text-center"><?= getconfig('text_loading_preview') ?></div>
+        <div id="prepare-wait" style="font-size: 200%; display: none;" class="container text-center"><?= getconfig('text_wait'); ?></div>
     </div>
     <video autoplay="true" id="camPreview"></video>
         <div class="container bottom-bar">
-        <div class="error"><?php if($_GET['error']) echo $_GET['error'] . '. Bitte versuche es noch einmal!'; ?></div>
+        <?php if($_GET['error']) : ?>
+            <div class="error"><?= getconfig('text_error') ?></div>
+            <div style="font-size:50%"><?= $_GET['error']; ?></div>
+            <?php endif; ?>
 
-            <a class="<?= $btnClass ?>" href="#" onclick="javascript:take();$('.error').hide();void(0)" id="countdown">Foto aufnehmen</a>
+            <a class="<?= $btnClass ?>" href="#" onclick="javascript:take();$('.error').hide();void(0)" id="countdown"><?= getconfig('text_take_photo') ?></a>
         </div>
     <script>
         $( document ).ready(function() {
@@ -236,66 +259,10 @@ function getCameraImage() {
                     });
             }
         });
-        function take() {
-            let countdown = <?= getconfig('countdown'); ?>;
-            var counter = countdown;
-            var countdownEl = $('#countdown');
-            countdownEl.text(counter);
-            var interval = setInterval(function() {
-                counter--;
-                countdownEl.text(counter);
-                if (counter == 0) {
-                    clearInterval(interval);
-                    // take photo
-                    countdownEl.text('Cheeeese!');
-                    capture();
-                }
-            }, 1000);
-        }
-        function capture() {
-            $.ajax({
-                url: "?capture",
-            }).done(function(data) {
-                console.log(data);
-                if(data.error) {
-                    console.log('ERROR', data.content);
-                    location.href = '?take&error=' + data.content;
-                }
-                else {
-                    prepare();
-                }
-            });
-        }
-        function prepare() {
-            $('#countdown').text('Verarbeitung...');
-            $('#camPreview').hide();
-            $('#preview-wait').hide();
-            $('#prepare-wait').show();
-            $.ajax({
-                url: "?prepare",
-            }).done(function(data) {
-                console.log(data);
-                if(data.error) {
-                    console.log('ERROR', data.content);
-                    location.href = '?take&error=' + data.content;
-                }
-                else {
-                    location.href = '?photo&src=' + data.content;
-                }
-            });
-        }
-        function flashdown() {
-            $.ajax({
-                url: "?flashdown",
-            }).done(function(data) {
-                console.log(data);
-            });
-            //location.href = '?photo';
-        }
     </script>
 
 <?php elseif(isset($_GET['photo'])) :  ?>
-    <meta http-equiv="refresh" content="<?= getconfig('refresh'); ?>; URL=/">
+    <meta http-equiv="refresh" content="<?= getconfig('refresh_photo'); ?>; URL=/">
     <div id="photo" style="background-image:url('/images/thumb/<?= $_GET['src'] ?>');" ></div>
     <div class="container bottom-bar">
         <a class="<?= $btnClass ?>" href="?take">Neues Foto</a>&nbsp;
@@ -310,46 +277,113 @@ function getCameraImage() {
             //upload();
             qrCode('dummy');
         });
-
-        function qrCode(link) {
-            var qrEl = $('#qrcode');
-            if(qrEl.data('generated') != 1) {
-                var qrcode = new QRCode(document.getElementById("qrcode"), {
-                    /*text: link,*/
-                    text: '<?= webUrl(getconfig('web_url_base'), $_GET['src']); ?>',
-                    width : 300,
-                    height : 300,
-                    useSVG: true
-                });
-                qrEl.data('generated', 1);
-                $('#qrbutton').show();
-            }
-        }
-
-        function qrToggle() {
-            $('#qrcode').toggle();
-        }
-
-        function upload() {
-            $.ajax({
-                url: "?upload&src=<?= $_GET['src']; ?>",
-            }).done(function(data) {
-                console.log(data);
-                qrCode(data);
-            });
-            //location.href = '?photo';
-        }
-
-        function flashdown() {
-            $.ajax({
-                url: "?flashdown",
-            }).done(function(data) {
-                console.log(data);
-            });
-        }
     </script>
 <?php endif; ?>
 
 <a id="home" href="/" class="btn">-</a>
+<script>
+    function take() {
+        let countdown = <?= getconfig('countdown'); ?>;
+        var counter = countdown;
+        var countdownEl = $('#countdown');
+        countdownEl.text(counter);
+        var interval = setInterval(function() {
+            counter--;
+            countdownEl.text(counter);
+            if (counter <= 0) {
+                clearInterval(interval);
+                // take photo
+                countdownEl.text('<?= getconfig('text_cheese') ?>');
+                capture();
+            }
+        }, 1000);
+    }
+    function capture() {
+        flashup();
+        $.ajax({
+            url: "?capture",
+        }).done(function(data) {
+            console.log(data);
+            if(data.error) {
+                console.log('ERROR', data.content);
+                location.href = '?take&error=' + data.content;
+            }
+            else {
+                prepare();
+            }
+        });
+    }
+    function prepare() {
+        $('#countdown').text('<?= getconfig('text_processing') ?>');
+        $('#camPreview').hide();
+        $('#preview-wait').hide();
+        $('#prepare-wait').show();
+        $.ajax({
+            url: "?prepare",
+        }).done(function(data) {
+            console.log(data);
+            if(data.error) {
+                console.log('ERROR', data.content);
+                location.href = '?take&error=' + data.content;
+            }
+            else {
+                location.href = '?photo&src=' + data.content;
+            }
+        });
+    }
+    function flashdown() {
+        $.ajax({
+            url: "?flashdown",
+        }).done(function(data) {
+            console.log(data);
+        });
+        //location.href = '?photo';
+    }
+    function qrCode(link) {
+        var qrEl = $('#qrcode');
+        if(qrEl.data('generated') != 1) {
+            var qrcode = new QRCode(document.getElementById("qrcode"), {
+                /*text: link,*/
+                text: '<?= webUrl(getconfig('web_url_base'), $_GET['src']); ?>',
+                width : 300,
+                height : 300,
+                useSVG: true
+            });
+            qrEl.data('generated', 1);
+            $('#qrbutton').show();
+            console.log('qr', '<?= webUrl(getconfig('web_url_base'), $_GET['src']); ?>');
+        }
+    }
+
+    function qrToggle() {
+        $('#qrcode').toggle();
+    }
+
+    function upload() {
+        $.ajax({
+            url: "?upload&src=<?= $_GET['src']; ?>",
+        }).done(function(data) {
+            console.log(data);
+            qrCode(data);
+        });
+        //location.href = '?photo';
+    }
+
+    function flashup() {
+        $.ajax({
+            url: "?flashup",
+        }).done(function(data) {
+            console.log(data);
+        });
+    }
+
+    function flashdown() {
+        $.ajax({
+            url: "?flashdown",
+        }).done(function(data) {
+            console.log(data);
+        });
+    }
+</script>
 </body>
 </html>
